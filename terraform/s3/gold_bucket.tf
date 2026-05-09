89a28e8d43821bb0ba02dca5c269abc9654feec7
Prakhar-ui <70754872+Prakhar-ui@users.noreply.github.com>
2026-05-09 18:32:33 +0530

    fixed bronze, silver and gold buckets

--- terraform/s3/gold_bucket.tf
+++ terraform/s3/gold_bucket.tf
@@ -1,26 +1,11 @@
-terraform {
-  required_version = ">= 1.5.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "ap-south-1"
-}
-
-# Create S3 Bucket
-resource "aws_s3_bucket" "bucket" {
+# Create Gold S3 Bucket
+resource "aws_s3_bucket" "gold_bucket" {
   bucket = "yt-data-pipeline-gold-prakhar"
 }
 
-# Enable Server Side Encryption
-resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
-  bucket = aws_s3_bucket.bucket.id
+# Enable Encryption
+resource "aws_s3_bucket_server_side_encryption_configuration" "gold_encryption" {
+  bucket = aws_s3_bucket.gold_bucket.id
 
   rule {
     apply_server_side_encryption_by_default {
@@ -30,16 +15,11 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
 }
 
 # Block Public Access
-resource "aws_s3_bucket_public_access_block" "public_access" {
-  bucket = aws_s3_bucket.bucket.id
+resource "aws_s3_bucket_public_access_block" "gold_public_access" {
+  bucket = aws_s3_bucket.gold_bucket.id
 
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
-}
-
-# Output bucket name
-output "bucket_name" {
-  value = aws_s3_bucket.bucket.bucket
 }
--- terraform/s3/silver_bucket.tf
+++ terraform/s3/silver_bucket.tf
@@ -1,26 +1,11 @@
-terraform {
-  required_version = ">= 1.5.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "ap-south-1"
-}
-
-# Create S3 Bucket
-resource "aws_s3_bucket" "bucket" {
+# Create Silver S3 Bucket
+resource "aws_s3_bucket" "silver_bucket" {
   bucket = "yt-data-pipeline-silver-prakhar"
 }
 
-# Enable Server Side Encryption
-resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
-  bucket = aws_s3_bucket.bucket.id
+# Enable Encryption
+resource "aws_s3_bucket_server_side_encryption_configuration" "silver_encryption" {
+  bucket = aws_s3_bucket.silver_bucket.id
 
   rule {
     apply_server_side_encryption_by_default {
@@ -30,16 +15,11 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
 }
 
 # Block Public Access
-resource "aws_s3_bucket_public_access_block" "public_access" {
-  bucket = aws_s3_bucket.bucket.id
+resource "aws_s3_bucket_public_access_block" "silver_public_access" {
+  bucket = aws_s3_bucket.silver_bucket.id
 
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
-}
-
-# Output bucket name
-output "bucket_name" {
-  value = aws_s3_bucket.bucket.bucket
 }
