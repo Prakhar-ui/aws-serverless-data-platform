24936d9cb2b843a504f77c876b1d04104c238d60
Prakhar-ui <70754872+Prakhar-ui@users.noreply.github.com>
2026-05-09 17:31:23 +0530

    fixed bucket name output

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,6 +14,37 @@ provider "aws" {
 }
 
 # Create S3 Bucket
+resource "aws_s3_bucket" "bronze_bucket" {
+  bucket = "yt-data-pipeline-bronze-prakhar"
+}
+
+# Enable Server Side Encryption
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  bucket = aws_s3_bucket.bronze_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# Block Public Access
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.bronze_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# Output bucket name
+output "bronze_bucket_name" {
+  value = aws_s3_bucket.bronze_bucket.bucket
+}
+
+# Create S3 Bucket
 resource "aws_s3_bucket" "silver_bucket" {
   bucket = "yt-data-pipeline-silver-prakhar"
 }
@@ -40,7 +71,7 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
 }
 
 # Output bucket name
-output "bucket_name" {
+output "silver_bucket_name" {
   value = aws_s3_bucket.silver_bucket.bucket
 }
 
@@ -71,37 +102,6 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
 }
 
 # Output bucket name
-output "bucket_name" {
+output "gold_bucket_name" {
   value = aws_s3_bucket.gold_bucket.bucket
-}
-
-# Create S3 Bucket
-resource "aws_s3_bucket" "bronze_bucket" {
-  bucket = "yt-data-pipeline-bronze-prakhar"
-}
-
-# Enable Server Side Encryption
-resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
-  bucket = aws_s3_bucket.bronze_bucket.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
-
-# Block Public Access
-resource "aws_s3_bucket_public_access_block" "public_access" {
-  bucket = aws_s3_bucket.bronze_bucket.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
-# Output bucket name
-output "bucket_name" {
-  value = aws_s3_bucket.bronze_bucket.bucket
 }
