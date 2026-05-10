db99c6096374d4cd4bb761761ec4cfb13a6af724
Prakhar-ui <70754872+Prakhar-ui@users.noreply.github.com>
2026-05-10 15:53:11 +0530

    fixed formatting

--- terraform/iam/backend.tf
+++ terraform/iam/backend.tf
@@ -1,9 +1,9 @@
 terraform {
-    backend "s3" {
-        bucket         = "yt-terraform-state-prakhar"
-        key            = "iam/terraform.tfstate"
-        region         = "ap-south-1"
-        dynamodb_table = "terraform-locks"
-        encrypt        = true
-    }
+  backend "s3" {
+    bucket         = "yt-terraform-state-prakhar"
+    key            = "iam/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
 }
