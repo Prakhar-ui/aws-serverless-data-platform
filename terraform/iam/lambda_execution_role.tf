5b0cb57fe233d13837be9d64042bd1cd0214319e
Prakhar-ui <70754872+Prakhar-ui@users.noreply.github.com>
2026-05-10 16:11:02 +0530

    fix execution role

--- terraform/iam/lambda_execution_role.tf
+++ terraform/iam/lambda_execution_role.tf
@@ -1,4 +1,32 @@
-# Inline Policy for Bronze S3 Bucket Access
+resource "aws_iam_role" "lambda_role" {
+  name = "yt-data-pipeline-lambda-role-dev"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = {
+    Name        = "yt-data-pipeline-lambda-role-dev"
+    Environment = "dev"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
 resource "aws_iam_role_policy" "lambda_bronze_s3_policy" {
   name = "lambda-bronze-s3-access-policy"
   role = aws_iam_role.lambda_role.id
