afb90d9f4834f1fa8694ddb73638d004901644dc
Prakhar-ui <prakha8380@gmail.com>
2026-05-13 20:59:43 +0530

    changed lambda backend from iam key to lambda key

--- terraform/lambda/backend.tf
+++ terraform/lambda/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "yt-terraform-state-prakhar"
-    key            = "iam/terraform.tfstate"
+    key            = "lambda/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
