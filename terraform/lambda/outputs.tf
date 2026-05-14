c0c59ef4ab5e48b21c43493ef0493e5e390958c8
Prakhar-ui <70754872+Prakhar-ui@users.noreply.github.com>
2026-05-14 16:59:19 +0530

    fixed lambda function output

--- terraform/lambda/outputs.tf
+++ terraform/lambda/outputs.tf
@@ -1,11 +1,23 @@
-output "lambda_function_name" {
-  value = aws_lambda_function.yt_lambda.function_name
+output "youtube_api_integstion_function_name" {
+  value = aws_lambda_function.youtube_api_integstion_function.function_name
 }
 
-output "lambda_function_arn" {
-  value = aws_lambda_function.yt_lambda.arn
+output "youtube_api_integstion_function_arn" {
+  value = aws_lambda_function.youtube_api_integstion_function.arn
 }
 
-output "lambda_invoke_arn" {
-  value = aws_lambda_function.yt_lambda.invoke_arn
+output "youtube_api_integstion_function_invoke_arn" {
+  value = aws_lambda_function.youtube_api_integstion_function.invoke_arn
+}
+
+output "json_to_parquet_function_name" {
+  value = aws_lambda_function.json_to_parquet_function.function_name
+}
+
+output "json_to_parquet_function_arn" {
+  value = aws_lambda_function.json_to_parquet_function.arn
+}
+
+output "json_to_parquet_function_invoke_arn" {
+  value = aws_lambda_function.json_to_parquet_function.invoke_arn
 }
