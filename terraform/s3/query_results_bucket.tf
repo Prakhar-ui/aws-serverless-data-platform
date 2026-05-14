a3c59ef49d07994d05351adcab1d7d6c47f3b13b
Prakhar-ui <prakha8380@gmail.com>
2026-05-14 08:44:19 +0530

    added new query result bucket

---
+++ terraform/s3/query_results_bucket.tf
@@ -0,0 +1,25 @@
+# Create Query Result S3 Bucket
+resource "aws_s3_bucket" "query_result_bucket" {
+  bucket = "yt-data-pipeline-query-result-prakhar"
+}
+
+# Enable Encryption
+resource "aws_s3_bucket_server_side_encryption_configuration" "query_result_encryption" {
+  bucket = aws_s3_bucket.query_result_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# Block Public Access
+resource "aws_s3_bucket_public_access_block" "query_result_public_access" {
+  bucket = aws_s3_bucket.query_result_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
