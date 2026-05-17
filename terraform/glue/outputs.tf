6dce3fa9e52436c4121dfd65c61fe535d43272ca
Prakhar-ui <70754872+Prakhar-ui@users.noreply.github.com>
2026-05-17 08:02:27 +0530

    added proper outputs for glue script and s3 uploader

--- terraform/glue/outputs.tf
+++ terraform/glue/outputs.tf
@@ -17,3 +17,35 @@ output "bronze_crawler_name" {
 output "bronze_crawler_arn" {
   value = aws_glue_crawler.bronze_crawler.arn
 }
+
+output "bronze_to_silver_statistics_glue_job_name" {
+  value = aws_glue_job.bronze_to_silver_statistics_glue_job.name
+}
+
+output "bronze_to_silver_statistics_glue_job_arn" {
+  value = aws_glue_job.bronze_to_silver_statistics_glue_job.arn
+}
+
+output "silver_to_gold_statistics_glue_job_name" {
+  value = aws_glue_job.silver_to_gold_statistics_glue_job.name
+}
+
+output "silver_to_gold_statistics_glue_job_arn" {
+  value = aws_glue_job.silver_to_gold_statistics_glue_job.arn
+}
+
+output "bronze_to_silver_statistics_glue_script_s3_key" {
+  value = aws_s3_object.bronze_to_silver_statistics_glue_script.key
+}
+
+output "bronze_to_silver_statistics_glue_script_s3_uri" {
+  value = "s3://${aws_s3_object.bronze_to_silver_statistics_glue_script.bucket}/${aws_s3_object.bronze_to_silver_statistics_glue_script.key}"
+}
+
+output "silver_to_gold_analytics_glue_script_s3_key" {
+  value = aws_s3_object.silver_to_gold_analytics_glue_script.key
+}
+
+output "silver_to_gold_analytics_glue_script_s3_uri" {
+  value = "s3://${aws_s3_object.silver_to_gold_analytics_glue_script.bucket}/${aws_s3_object.silver_to_gold_analytics_glue_script.key}"
+}
