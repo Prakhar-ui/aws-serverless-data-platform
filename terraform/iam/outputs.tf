1aa81ef460c04fde8affa078ec97ff13a9c0d9b0
Prakhar-ui <70754872+Prakhar-ui@users.noreply.github.com>
2026-05-17 11:23:06 +0530

    added step function to iam outputs

--- terraform/iam/outputs.tf
+++ terraform/iam/outputs.tf
@@ -2,6 +2,22 @@ output "lambda_iam_role_arn" {
   value = aws_iam_role.lambda_role.arn
 }
 
+output "lambda_iam_role_name" {
+  value = aws_iam_role.lambda_role.name
+}
+
 output "glue_iam_role_arn" {
   value = aws_iam_role.glue_role.arn
+}
+
+output "glue_iam_role_name" {
+  value = aws_iam_role.glue_role.name
+}
+
+output "step_function_role_arn" {
+  value = aws_iam_role.step_function_role.arn
+}
+
+output "step_function_role_name" {
+  value = aws_iam_role.step_function_role.name
 }
